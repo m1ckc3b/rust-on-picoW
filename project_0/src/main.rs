1ab42b5f2d7d84869f5a2e2797554c2880f02a54
mickceb <cebulski.mi@gmail.com>
2025-01-23 12:41:05 +0100

    remove "can't find crate for test" error

--- project_0/src/main.rs
+++ project_0/src/main.rs
@@ -1,6 +1,3 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP22,
 #![no_std]
 #![no_main]
 
@@ -10,7 +7,6 @@ use defmt_rtt as _;
 use embedded_hal::digital::OutputPin;
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
 use rp_pico as bsp;
 
 use bsp::hal::{
@@ -28,7 +24,6 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
@@ -62,5 +57,3 @@ fn main() -> ! {
         delay.delay_ms(500);
     }
 }
-
-// End of file
